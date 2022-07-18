18b9c6bec97ccd83d2538f11c24cda3754c647ae
mstars_lab3_01 <mstars_lab3_01@mstars-lab3-01s-iMac.local>
2022-07-18 10:58:45 +0800

    [add]

--- Back-End/profile.js
+++ Back-End/profile.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+require("dotenv").config();
+
+app.use(express.static("public"));
 
 const cors = require("cors");
 app.use(
@@ -9,9 +12,9 @@ app.use(
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
-
+console.log(process.env.PORT);
 app.get("/api", cors(), (req, res) => {
-  fs.readFile("./data/profile.json", "utf-8", (err, data) => {
+  fs.readFile("./models/profile.json", "utf-8", (err, data) => {
     if (err) {
       console.log("error on reading");
     } else {
